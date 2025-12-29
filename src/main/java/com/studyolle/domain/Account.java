2e2c69ecd111a107d38c48d8c564d5248b4a0f58
lhr5767 <ghkfuf789@naver.com>
2025-12-29 20:31:16 +0900

    스터디올래 프로젝트 시작 - 계정 관리 기능
    1.프로젝트 셋팅후 계정 (Account) 엔티티 추가

---
+++ src/main/java/com/studyolle/domain/Account.java
@@ -0,0 +1,57 @@
+package com.studyolle.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified; //이메일 인증 여부
+
+    private String emailCheckToken; //이메일 검증시 사용할 토큰
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb;
+}
