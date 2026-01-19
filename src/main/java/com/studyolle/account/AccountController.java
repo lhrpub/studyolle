71087a12a368adfbff2bcec194d88e02a4bcc6bf
leehr <dlghkfuf@naver.com>
2026-01-20 00:12:15 +0900

    회원가입 form 검증 및 가입처리

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -1,15 +1,63 @@
 package com.studyolle.account;
 
+import com.studyolle.domain.Account;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model){
         model.addAttribute("signUpForm" , new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String singUpSubmit(@Valid SignUpForm signUpForm , Errors errors){
+        if (errors.hasErrors()){
+            return "account/sign-up";
+        }
+
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) // encoding 처리 해야함
+                .studyCreatedByWeb(true)
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
+        Account newAccount = accountRepository.save(account);
+
+        newAccount.generateEmailCheckToken();
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(newAccount.getEmail());
+        simpleMailMessage.setSubject("스터디올래, 회원 가입 인증");
+        simpleMailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());
+        javaMailSender.send(simpleMailMessage);
+
+
+        //todo 회원 가입 처리
+        return "redirect:/";
     }
 }
