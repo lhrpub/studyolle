56db8eb4457a5cbd2025b25a36d365d795bdad18
lhr5767 <ghkfuf789@naver.com>
2026-03-16 16:40:32 +0900

    타임존 설정

--- src/main/java/com/studyolle/chat/dto/ChatMessageDTO.java
+++ src/main/java/com/studyolle/chat/dto/ChatMessageDTO.java
@@ -1,5 +1,6 @@
 package com.studyolle.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.studyolle.chat.entity.ChatMessage;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +16,7 @@ public class ChatMessageDTO {
     private Long senderId;
     private String senderName;
     private String message;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime sentAt;
 
     public static ChatMessageDTO from(ChatMessage entity){
